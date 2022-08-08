25c43d4b2398acb7a47d281e1ee6192494070877
miray <miray.mutlu@ozu.edu.tr>
2022-08-08 14:13:23 +0300

    buttons updated for small devices

--- src/components/galleryComponents/Buttons.jsx
+++ src/components/galleryComponents/Buttons.jsx
@@ -5,13 +5,13 @@ import InfoIcon from "@mui/icons-material/Info";
 function Buttons() {
   return (
     <span>
-      <button className="px-2 py-2 bg-zinc-200 text-black mr-2 my-2 rounded-md w-20 place-content-center">
+      <button className="xs:px-1 xs:py-1 sm:px-2 sm:py-2 bg-zinc-200 text-black my-2 rounded-md w-19 place-content-center">
         <PlayArrowIcon />
-        <span className="ml-2 font-medium">Play</span>
+        <span className="ml-2 xs:text-xs sm:text-sm md:text-md">Play</span>
       </button>
-      <button className="px-2 py-2 bg-zinc-400 text-white rounded-md w-30 place-content-center">
+      <button className="xs:px-1 xs:py-1 md:px-2 md:py-2 bg-zinc-400 text-white rounded-md w-30 place-content-center">
         <InfoIcon />
-        <span className="ml-2 sm:font-sm md:font-medium lg:font-medium">More information</span>
+        <span className="ml-2 xs:text-xs sm:text-sm md:text-md">More information</span>
       </button>
     </span>
   );
