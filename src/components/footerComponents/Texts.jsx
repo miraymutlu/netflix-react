7d95bf4011e1c1b085f96e7ced0cb3b96f4f8bf8
miray <miray.mutlu@ozu.edu.tr>
2022-08-09 11:36:44 +0300

    footer updated

--- src/components/footerComponents/Texts.jsx
+++ src/components/footerComponents/Texts.jsx
@@ -4,8 +4,8 @@ function Texts(props) {
   return (
     <div>
       <div className="bg-black text-gray-600 font-bold h-24">
-        <ul className="mx-10 flex flex-wrap cursor-pointer">
-          {props.data?.map(x=>(<li key={x.id} className="basis-1/4">{x.text}</li>))}
+        <ul className="mx-10 flex flex-wrap cursor-pointer place-items-center text-xs">
+          {props.data?.map(x=>(<li key={x.id} className="mt-2 mr-2  basis-4/7">{x.text}</li>))}
         </ul>
       </div>
     </div>
