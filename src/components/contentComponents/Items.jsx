40f3ebb7d97d3b6959bc1d9f7f5b8a7d73703278
miray <miray.mutlu@ozu.edu.tr>
2022-08-09 12:22:09 +0300

    new flms added

--- src/components/contentComponents/Items.jsx
+++ src/components/contentComponents/Items.jsx
@@ -3,7 +3,7 @@ import "./items.css"
 
 function Items(props) {
   return (
-    <div className="container relative cursor-pointer border-4 border-black h-40 sm:h-32 lg:h-56">
+    <div className="container relative cursor-pointer border-4 border-black h-40 sm:h-32 lg:h-36 xl:h-36 2xl:h-60">
       <div className= "item">
         <img
           src={props.image.imageURL}
